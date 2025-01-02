a288c75c66f5197c9a987d11fe69bf354a55bbaa
svenwal <sven@svenwal.de>
2025-01-02 12:56:34 +0100

    First commit for JSON schema format

--- kong/plugins/odata-validation/schema.lua
+++ kong/plugins/odata-validation/schema.lua
@@ -4,7 +4,18 @@ return {
     { config = {
         type = "record",
         fields = {
-          { odata_specification = { type = "string", required = true } },
+          { odata_specification = { 
+              type = "string", 
+              required = true,
+              description = "OData specification in XML or JSON format" 
+          }},
+          { specification_format = {
+              type = "string",
+              required = false,
+              default = "auto",
+              one_of = { "auto", "xml", "json" },
+              description = "Format of the OData specification - auto will try to detect based on content"
+          }},
         },
       },
     },
